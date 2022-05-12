725f543871e125527f1d89e81d7c2141a5e8d151
Lincoln <lincolny@ngis.com.au>
2022-05-12 20:56:08 +0800

    add card-list

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import { Component } from "react";
+import "./App.css";
+
+import CardList from "./components/card-list/card-list.component";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(() => {
+          return { monsters: users };
+        })
+      );
+  }
+
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(()=>{
+      return {searchField}
+    })
+
+  }
+
+  render() {
+
+    console.log('render');
+
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    })
+
+    return (
+      <div className="App">
+          <input
+            className="search-box"
+            type="search"
+            placeholder="search monsters"
+            onChange={onSearchChange}
+          />
+          <CardList monsters={filteredMonsters}/>
+
+          {/* <span className="App-link">Learn React with typescript</span> */}
+      </div>
+    );
+  }
+}
+
+export default App;
+
