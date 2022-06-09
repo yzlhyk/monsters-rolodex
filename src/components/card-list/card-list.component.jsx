5c34638fb3fafb2dbc5fd9e199ce4e21faa00441
Lincoln <lincolny@ngis.com.au>
2022-06-09 21:47:24 +0800

    60. convert rest of components

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -2,20 +2,12 @@ import { Component } from "react";
 import Card from "../card/card.component";
 import "./card-list.styles.css";
 
-class CardList extends Component {
-  render() {
-    // console.log(this.props.monsters);
-    const { monsters } = this.props;
-    // console.log(this.props);
-
-    return (
-      <div className="card-list">
-        {monsters.map((monster) => {
-          return <Card monster={monster} />;
-        })}
-      </div>
-    );
-  }
-}
+const CardList = ({ monsters }) => (
+  <div className="card-list">
+    {monsters.map((monster) => {
+      return <Card monster={monster} />;
+    })}
+  </div>
+);
 
 export default CardList;
